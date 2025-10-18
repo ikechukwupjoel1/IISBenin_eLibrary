899fba99c6ff1120fac396bb949f691ae702a7e2
ikechukwupjoel1 <iksotech@gmail.com>
2025-10-18 16:46:43 +0100

    chore(vercel): add SPA rewrite config for Vite React app

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <App />
+      <SpeedInsights />
     </AuthProvider>
   </StrictMode>
 );
